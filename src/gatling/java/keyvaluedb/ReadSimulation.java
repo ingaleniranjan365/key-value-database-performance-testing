133ef5e0458be2381fc13c971bc1b2a6759a31bd
ingaleniranjan365 <ingaleniranjan365@gmail.com>
2023-08-12 14:52:42 +0530

    Adding following simulations for perf testing a key-value database 1. Write key-value pairs 2. Read key-value pairs 3. Write and then read, database is hit with both operations simultaneously. Simulation is configured to perform read after one second for data written/inserted now. 4. Data used is NYC Taxi Trips data, for more details on data refer to https://www.kaggle.com/competitions/nyc-taxi-trip-duration/data

---
+++ src/gatling/java/keyvaluedb/ReadSimulation.java
@@ -0,0 +1,36 @@
+package keyvaluedb;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+public class ReadSimulation extends Simulation {
+
+        int applicationPort = 8081;
+        FeederBuilder<String> feeder = csv("trips.csv").circular();
+
+        ScenarioBuilder retrieve = scenario("Retrieve")
+                .feed(feeder)
+                .exec(
+                        http("retrieve")
+                                .get("/latest/element/#{id}")
+                                .check(
+                                        status().is(200)
+                                )
+                );
+
+
+        HttpProtocolBuilder httpProtocol =
+                http.baseUrl("http://localhost:" + applicationPort)
+                        .acceptHeader("text/html,application/json")
+                        .acceptEncodingHeader("gzip, deflate");
+
+        {
+                setUp(
+                        retrieve.injectOpen(rampUsers(5).during(3))
+                ).protocols(httpProtocol);
+        }
+
+}
---
+++ src/gatling/java/keyvaluedb/WriteReadSimulation.java
@@ -0,0 +1,89 @@
+package keyvaluedb;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import java.util.Objects;
+
+import java.util.function.Function;
+
+public class WriteReadSimulation extends Simulation {
+
+        int applicationPort = 8081;
+        FeederBuilder<String> feeder = csv("trips.csv").circular();
+
+        ScenarioBuilder insertAndRetrieve = scenario("Insert")
+                .feed(feeder)
+                .exec(
+                        http("insert")
+                                .put(session -> {
+                                        var timestamp = Objects.requireNonNull(session.getString("dropoff_datetime"))
+                                                .replace(" ", "T") + 'Z';
+                                        var id= session.getString("id");
+                                        return String.format("/element/%s/timestamp/%s", id, timestamp);
+                                })
+                                .body(io.gatling.javaapi.core.CoreDsl.StringBody(RequestBodyBuilder.extractJSONString))
+                                .check(
+                                        status().is(200)
+                                )
+                )
+                .pause("1")
+                .exec(
+                        http("retrieve")
+                                .get("/latest/element/#{id}")
+                                .check(
+                                        status().is(200)
+                                )
+                );
+
+
+        HttpProtocolBuilder httpProtocol =
+                http.baseUrl("http://localhost:" + applicationPort)
+                        .acceptHeader("text/html,application/json")
+                        .acceptEncodingHeader("gzip, deflate");
+
+        {
+                setUp(
+                        insertAndRetrieve.injectOpen(rampUsers(5).during(3))
+                ).protocols(httpProtocol);
+        }
+
+        static final class RequestBodyBuilder {
+                public static final Function<Session, String> extractJSONString = session -> {
+
+                        int vendorId = session.getInt("vendor_id");
+                        int passengerCount = session.getInt("passenger_count");
+                        double pickupLongitude = session.getDouble("pickup_longitude");
+                        double pickupLatitude = session.getDouble("pickup_latitude");
+                        double dropoffLongitude = session.getDouble("dropoff_longitude");
+                        double dropoffLatitude = session.getDouble("dropoff_latitude");
+                        String id = session.getString("id");
+                        String pickupDatetime = Objects.requireNonNull(session.getString("pickup_datetime"))
+                                .replace(" ", "T")+'Z';
+                        String dropoffDatetime = Objects.requireNonNull(session.getString("dropoff_datetime"))
+                                .replace(" ", "T")+'Z';
+                        String storeAndFwdFlag = session.getString("store_and_fwd_flag");
+                        String tripDuration = session.getString("trip_duration");
+
+                        String jsonString = "{"
+                                + "\"id\":\"" + id + "\","
+                                + "\"vendor_id\":" + vendorId + ","
+                                + "\"pickup_datetime\":\"" + pickupDatetime + "\","
+                                + "\"dropoff_datetime\":\"" + dropoffDatetime + "\","
+                                + "\"passenger_count\":" + passengerCount + ","
+                                + "\"pickup_longitude\":" + pickupLongitude + ","
+                                + "\"pickup_latitude\":" + pickupLatitude + ","
+                                + "\"dropoff_longitude\":" + dropoffLongitude + ","
+                                + "\"dropoff_latitude\":" + dropoffLatitude + ","
+                                + "\"store_and_fwd_flag\":\"" + storeAndFwdFlag + "\","
+                                + "\"trip_duration\":\"" + tripDuration + "\""
+                                + "}";
+
+                        return jsonString;
+                };
+        }
+
+}
